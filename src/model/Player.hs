48ab67553978b61545dbf962f11e66e863b4c3c0
Bram Boogaarts <bram.boogaarts@gmail.com>
2023-10-31 09:38:13 +0100

    Apply bounds in movePlayer, add playerIsHit

--- src/model/Player.hs
+++ src/model/Player.hs
@@ -1,7 +1,8 @@
 module Model.Player where
 
 import Model.General (Position, Vector, HasPosition, pos, move)
-import Model.Shooting (Weapon, shootWeapon, CanShoot, shoot)
+import Model.Parameters
+import Model.Shooting (Bullet, Weapon, shootWeapon, CanShoot, shoot)
 
 data Player = Player {
   playerPos :: Position,
@@ -16,8 +17,14 @@ instance HasPosition Player where
 instance CanShoot Player where
   shoot p = shootWeapon (weapon p) p True
 
--- player only moves in y
+-- player only moves in y, cannot move beyond max and min y
 movePlayer :: Player -> Float -> Player
 movePlayer pl dy = let p = playerPos pl
                        s = speed pl
-                    in pl {playerPos = move p (0, dy * s)}
+                       (x, y) = move p (0, dy * s)
+                    in pl {playerPos = (x, min (max y screenMin) screenMax)}
+
+-- Determine if a bullet hits a player. Can use later to despawn the bullet and subtract a life from the player.
+playerIsHit :: Player -> Bullet -> Maybe Bullet
+playerIsHit p b = if pos p == pos b then Just b
+                                    else Nothing
