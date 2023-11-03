e3526c720c081faeba94a2ef71105d57dfe5eebb
Yannaya3 <y.i.vanstraten@students.uu.nl>
2023-11-03 12:29:29 +0100

    Created input function, moving player up and down.

--- src/controller/Controller.hs
+++ src/controller/Controller.hs
@@ -1,5 +1,15 @@
 module Controller.Controller where
+import Graphics.Gloss.Interface.IO.Game
+import Model.GameState
+import Model.Player
 
 -- This function takes an event, a certain keyboard input, and a current gamestate, and returns the new gamestate.
 keyboardInputHandler :: Event -> GameState -> GameState
-keyboardInputHandler (EventKey (SpecialKey KeyUp)
+-- Move the player with the up and down arrow keys
+keyboardInputHandler (EventKey (SpecialKey KeyUp) Up _ _) gameState = gameState {player = moveUp player}
+keyboardInputHandler (EventKey (SpecialKey KeyUp) Down _ _) gameState = gameState {player = moveDown player}
+    where
+        -- Update the position of the player
+        moveUp player = movePlayer player verticalMovementStep
+        moveDown player = movePlayer player -verticalMovementStep
+        verticalMovementStep = 5
