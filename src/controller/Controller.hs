b7008a967e7e46300619f4afdebcd493cd797229
Yannaya3 <y.i.vanstraten@students.uu.nl>
2023-11-05 14:08:34 +0100

    Added all possible cases for other pressed keys.

--- src/controller/Controller.hs
+++ src/controller/Controller.hs
@@ -15,22 +15,25 @@ input _ = return
 
 -- This function takes an event, a certain keyboard input, and a current gamestate, and returns the new gamestate.
 keyboardInputHandler :: Event -> GameState -> GameState
-keyboardInputHandler (EventKey pressedButton _ _ _) gameState  = case phase gamestate of
+keyboardInputHandler (EventKey pressedButton _ _ _) gameState  = case phase gameState of
   -- If the gamephase is 'playing', the following input is considered:
   Playing -> case pressedButton of
-    SpecialKey KeyUp -> gameState {player =  movePlayer (player gameState) verticalMovementStep} -- Move the player with the upwards.
-    SpecialKey KeyDown -> gameState {player = movePlayer (player gameState) ((-1) * verticalMovementStep)} -- Move the player with the downwards.
-    Char r -> initalState -- Reset the gamestate to the initalGamestate by pressing r.
-    Char s -> gameState {bullets = bullets gameState ++ shoot (player gameState)} -- Shoot when 's' is pressed, update the current bullet list of the gamestate with the new bullet(s).
+    SpecialKey KeyUp -> gameState {player =  movePlayer (player gameState) verticalMovementStep} -- Move the player upwards.
+    SpecialKey KeyDown -> gameState {player = movePlayer (player gameState) ((-1) * verticalMovementStep)} -- Move the player downwards.
+    Char r -> initialState -- Reset the gamestate to the initalGamestate by pressing r.
+    Char s -> gameState {bullets = bullets gameState ++ snd (shoot (player gameState))} -- Shoot when 's' is pressed, update the current bullet list of the gamestate with the new bullet(s).
     Char p -> gameState {phase =  Paused} -- Pause the game.
+    _ -> gameState -- When another key is pressed, do nothing.
   -- When the game is paused, you can only reset the game, unpause or save the gamestate to JSON.
   Paused -> case pressedButton of
-    Char r -> initalState
+    Char r -> initialState
     Char p -> gameState {phase =  Playing} -- Unpause the game.
+    _ -> gameState
     -- Char q -> -- Save gameState to JSON.
   -- When game over, you can only reset the game.
   GameOver -> case pressedButton of
-    Char r -> initalState
+    Char r -> initialState
+    _ -> gameState
 
 -- Nothing happens if any other type of keyboard event is triggered.
 keyboardInputHandler _ gameState = gameState
