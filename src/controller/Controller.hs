0273e5595366b2d07d31d22ef7b4ee1a3e5a4948
Yannaya3 <y.i.vanstraten@students.uu.nl>
2023-11-05 13:53:44 +0100

    Added considered keys per gamePhase.

--- src/controller/Controller.hs
+++ src/controller/Controller.hs
@@ -16,25 +16,24 @@ input _ = return
 -- This function takes an event, a certain keyboard input, and a current gamestate, and returns the new gamestate.
 keyboardInputHandler :: Event -> GameState -> GameState
 keyboardInputHandler (EventKey pressedButton _ _ _) gameState  = case phase gamestate of
-  
-
-
-
-keyboardInputHandler (EventKey pressedButton _ _ _) gameState  = case pressedButton of
-  Char r -> initalGameState -- Reset the gamestate to the initalGamestate by pressing r, variable does not exist yet.
-  Char s -> gameState {bullets = bullets gameState ++ shoot (player gameState)} -- Shoot when 's' is pressed, update the current bullet list of the gamestate with the new bullet(s).
-  Char p -> gameState {isPaused = not (isPaused gameState)} -- Pause the game, or unpause if the game is already paused.
-  -- Char q -> if isPaused gameState = -- Save gameState to JSON -- else Nothing
-
-  -- Move the player with the up and down arrow keys
-  SpecialKey KeyUp -> gameState {player =  movePlayer (player gameState) verticalMovementStep}
-  SpecialKey KeyDown -> gameState {player = movePlayer (player gameState) ((-1) * verticalMovementStep)}
-
-  -- Nothing happens if any other key is pressed.
-  _ -> gameState
+  -- If the gamephase is 'playing', the following input is considered:
+  Playing -> case pressedButton of
+    SpecialKey KeyUp -> gameState {player =  movePlayer (player gameState) verticalMovementStep} -- Move the player with the upwards.
+    SpecialKey KeyDown -> gameState {player = movePlayer (player gameState) ((-1) * verticalMovementStep)} -- Move the player with the downwards.
+    Char r -> initalState -- Reset the gamestate to the initalGamestate by pressing r.
+    Char s -> gameState {bullets = bullets gameState ++ shoot (player gameState)} -- Shoot when 's' is pressed, update the current bullet list of the gamestate with the new bullet(s).
+    Char p -> gameState {gamePhase =  Paused} -- Pause the game.
+  -- When the game is paused, you can only reset the game, unpause or save the gamestate to JSON.
+  Paused -> case pressedButton of
+    Char r -> initalState
+    Char p -> gameState {gamePhase =  Playing} -- Unpause the game.
+    -- Char q -> -- Save gameState to JSON.
+  -- When game over, you can only reset the game.
+  GameOver -> case pressedButton of
+    Char r -> initalState
 
 -- Nothing happens if any other type of keyboard event is triggered.
 keyboardInputHandler _ gameState = gameState
 
 
-verticalMovementStep = 5.0
+verticalMovementStep = 5.0 -- Amount of vertical movement of player based on input.
