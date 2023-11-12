5121be49203e2a5673ba29e8b1010179d831837a
Bram <bram.boogaarts@gmail.com>
2023-11-12 18:01:32 +0100

    Removed unused function

--- src/model/GameState.hs
+++ src/model/GameState.hs
@@ -212,16 +212,6 @@ spawnNewPowerUp gs = do p <- generateProbability
                         if p <= spawnPowerUpOnStepProbability then spawnInGameState spawnPowerUp gs
                         else return gs
 
--- Take the animations from the queue and play the right type of animation at the right position.
-handleAnimationQueue :: GameStateTransformIO
-handleAnimationQueue gs@GameState{animations = []} = return gs -- Do nothing is there are no animations to be played.
-handleAnimationQueue gs@GameState{animations = (a1:as)} =
-    -- Otherwise, playe the next animation and the rest recursively.
-    do animate window black (playAnimation PowerUpAnimation(animationPos a1))
-       handleAnimationQueue gs {animations = as}
-       return gs
-
-
 -- Combine all GameStateTransforms that occur on a time step into one, if the game is in Playing phase.
 updateOnStep :: Float -> GameStateTransformIO
 updateOnStep t gs@GameState{phase} = case phase of
