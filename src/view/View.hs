f7a2b0566ac6ee02cb7e661bab2574e711d283fb
Yannaya3 <y.i.vanstraten@students.uu.nl>
2023-11-12 20:37:34 +0100

    Removed duplicate code.

--- src/view/View.hs
+++ src/view/View.hs
@@ -103,55 +103,6 @@ relativePos aPos@(x, y) relativeFactor direction = case direction of
     ToRight -> (x + relativeFactor, y)
     ToLeft -> (x - relativeFactor, y)
 
-
--- Given the gamestate, return a picture of the lives fo the player and their lives.
-pictureLivesAndScore :: GameState -> Picture
-pictureLivesAndScore gs@GameState{player = player, score = score} = Pictures [scorePicture, livesPicture]
-    where
-        scorePicture = scale textSize textSize $ translate (3.2 * screenMinX) (3 * screenMaxY) $ color white (Text ("Score:" ++ show score))-- Place the score in the left corner.
-        livesPicture = scale textSize textSize $ translate (2.5 * screenMinX) (3 * screenMaxY) $ color white (Text ("Lives:" ++ show (lives player))) -- The lives are to the right of the score.
-        textSize = 0.3
-
-
--- Given the gamestate with the animationqueue, return the current animation frames based on the difference between AnimationStart and elapsedTime.
-renderAnimations :: GameState -> Picture
-renderAnimations gs@GameState{animations} = Pictures $ map renderOneAnimation animations
-    where
-        renderOneAnimation :: Animation -> Picture
-        renderOneAnimation animation = renderParticles animation relativeFactor difference -- During the animation, show the particles at the right place.
-                                     
-            where
-                startingTime = animationStart animation
-                currentTime = elapsedTime gs
-                difference = currentTime - startingTime
-                -- This factor shows how far in the animation we are.
-                relativeFactor = animationSize * currentTime / animationLength
-
-
--- This function takes an animation and renders a particle there, where the position is based on the difference between elaspedTime and animationStart.
-renderParticles :: Animation -> Float -> Float -> Picture
-renderParticles animation relativeFactor difference = Pictures [topParticle, bottomParticle, leftParticle, rightParticle]
-    where
-        -- Based on the type of animation, give the corresponding shape and colour of the particles.
-        (aType, aPos@(x,y))  = (animationType animation, animationPos animation)
-        topParticle = uncurry translate (relativePos aPos relativeFactor ToTop) shapeAndColourParticles
-        bottomParticle = uncurry translate (relativePos aPos relativeFactor ToBottom) shapeAndColourParticles
-        leftParticle = uncurry translate (relativePos aPos relativeFactor ToLeft) shapeAndColourParticles
-        rightParticle = uncurry translate (relativePos aPos relativeFactor ToRight) shapeAndColourParticles
-        shapeAndColourParticles = case aType of
-            PowerUpAnimation -> color yellow $ circleSolid particleSize
-            BulletAnimation -> color blue $ circleSolid (particleSize/2)
-            DespawnAnimation -> color red $ rectangleSolid particleSize particleSize
-
--- Based on the direction and time since the start of the animation, return a relative position of a particle.
-relativePos :: Position -> Float -> Direction -> Position
-relativePos aPos@(x, y) relativeFactor direction = case direction of
-    ToTop -> (x, y + relativeFactor)
-    ToBottom -> (x, y - relativeFactor)
-    ToRight -> (x + relativeFactor, y)
-    ToLeft -> (x - relativeFactor, y)
-
-
 -- Return all the pictures of the entire gamestate.
 viewPure :: GameState -> Picture
 viewPure gs@GameState{ player, enemies = (basicEnemyList, burstEnemyList, coneEnemyList, basicPlayerSeekingEnemyList,fastPlayerSeekingEnemyList), bullets, powerUps} =
